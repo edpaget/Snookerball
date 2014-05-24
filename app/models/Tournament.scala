d1f002c6809a97b51453e229f193d88b9f0987ac
Edward Paget <edward@cassetteta.pe>
2014-05-23 21:03:30 -0500

    Adding Tournaments

---
+++ app/models/Tournament.scala
@@ -0,0 +1,3 @@
+package models
+
+case class Tournament(id: Option[Long], name: String, displayName: String)
---
+++ app/models/database/Tournaments.scala
@@ -0,0 +1,12 @@
+package models.database
+
+import play.api.db.slick.Config.driver.simple._
+import models.Tournament
+
+private[models] class Tournaments(tag: Tag) extends Table[Tournament](tag, "Tournaments") {
+  def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+  def name = column[String]("name")
+  def displayName = column[String]("display_name")
+  def * = (id.?, name, displayName) <> (Tournament.tupled, Tournament.unapply _)
+  def nameIdx = index("idx_name", name, unique = true)
+}
